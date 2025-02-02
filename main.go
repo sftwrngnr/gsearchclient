efa4469be024e2b9918b516c1ae4d8825505cf22
Dan Henderson <sftwrngnr66@gmail.com>
2025-02-02 01:05:27 -0700

    Update to reflect google search test.

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	googlesearch "github.com/rocketlaunchr/google-search"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
 )
 
 func main() {
-	ctx := context.Background()
-	opt := googlesearch.SearchOptions{CountryCode: "us"}
-	lret, err := googlesearch.Search(ctx, "cars for sale in queen creek,az", opt)
+	url := "https://www.google.com/search?q=go+tutorials&gl=us&hl=en"
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf(err.Error())
+		log.Fatal(err)
 	}
-	fmt.Println(lret)
-	fmt.Println(googlesearch.Search(ctx, "cars for sale in Toronto, Canada"))
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(doc.Find("body").Text())
 }
