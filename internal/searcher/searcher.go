2a9e2c1c4ce62d0001b972fb631cb929639ef1ff
Dan Henderson <sftwrngnr66@gmail.com>
2025-02-02 23:04:10 -0700

    Removed search key (doh)! New key recreated.

--- internal/searcher/searcher.go
+++ internal/searcher/searcher.go
@@ -14,7 +14,7 @@ func searchdata() {
 		"google_domain": "google.com",
 	}
 
-	search := g.NewGoogleSearch(parameter, "330b3fbe3c76655dfa738ffade25801d699dce3d45384a9a0643d89ba90c01df")
+	search := g.NewGoogleSearch(parameter, "mykey")
 	results, err := search.GetJSON()
 	if err != nil {
 		fmt.Println(err)
