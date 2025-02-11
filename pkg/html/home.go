ae89fbba5752d6a4d9dc034edb662831cee9ede0
Dan Henderson <sftwrngnr66@gmail.com>
2025-02-11 00:34:48 -0700

    Minor update and refactoring.

--- pkg/html/home.go
+++ pkg/html/home.go
@@ -33,6 +33,11 @@ func HomePage(items []string) Node {
 	)
 }
 
+func searchreqscript() (rval []Node) {
+	rval = append(rval, Script())
+	return
+}
+
 func getStateOptions() []Node {
 	rval := []Node{}
 	myStates, err := system.GetSystemParams().Dbc.GetAllStates()
--- pkg/html/zipcodes.go
+++ pkg/html/zipcodes.go
@@ -15,31 +15,25 @@ func ZipCodes(qs string) Node {
 	var rval []Node
 	var zval []Node
 	var acval []Node
+	var kwval []Node
 	fmt.Printf("ZipCode qs: %s\n", qs)
 	myZips, err := system.GetSystemParams().Dbc.GetZipsForState(qs)
 	if err != nil {
 		fmt.Printf("ZipCodes: %v\n", err)
 		return nil
 	}
-	/*
-		Div(ID("selectorupdate"),
-		Table(
-			Tr(Th(Text("Zip code")), Th(Text("Area code")), Th(Text("Keywords"))),
-			Tr(
-				Td(Div(Label(Name("zipcodes")), ID("zipcodes"))),
-				Td(Div(Label(Name("areacodes")), ID("areacodes"))),
-				Td(Div(Label(Name("keywords")), ID("keywords")))),
-		)),
-
-	*/
 	myAreaCodes, _ := system.GetSystemParams().Dbc.GetAreaCodesForState(qs)
 	fmt.Printf("There are %d area codes for qs: %s\n", len(myAreaCodes), qs)
-	//rval = append(rval, (ID("selectorupdate")))
+	myKeywords, _ := system.GetSystemParams().Dbc.GetKeywords()
+
 	for _, ac := range myAreaCodes {
 		acval = append(acval, Option(Value(ac.Code), Text(ac.Code)))
 	}
 	for _, z := range myZips {
 		zval = append(zval, Option(Value(strconv.Itoa(int(z.ID))), Text(z.Zipcode)))
+	}
+	for _, k := range myKeywords {
+		kwval = append(kwval, Option(Value(k.Keyword), Text(k.Keyword)))
 	}
 	rval = append(rval, Table(Tr(Th(Text("Zip code")), Th(Text("Area code")), Th(Text("Keywords"))),
 		Tr(
@@ -49,7 +43,17 @@ func ZipCodes(qs string) Node {
 			Td(Div(Label(Name("areacodes")), ID("areacodes")),
 				Select(Name("ac"), ID("ac"), Multiple(), Var(acval...)),
 			),
-		)))
+			Td(Div(Label(Name("keywords")), ID("keywords")),
+				Select(Name("kw"), ID("kw"), Multiple(), Var(kwval...)),
+			),
+		),
+		Tr(
+			Td(Input(Type("checkbox"), Label(Text("allzc"))), Text("All Zipcodes")),
+			Td(Input(Type("checkbox"), Label(Text("allac"))), Text("All Area Codes")),
+			Td(Input(Type("checkbox"), Label(Text("allkw"))), Text("All Keywords"),
+				Input(Type("checkbox"), Label(Text("indivkw"))), Text("Individual Keywords")),
+		),
+	))
 
 	return Var(rval...)
 }
