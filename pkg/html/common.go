3de09a4090137b58a821782d61f98980772c9cd3
Dan Henderson <sftwrngnr66@gmail.com>
2025-02-12 22:18:31 -0700

    Update to common.go.

--- pkg/html/common.go
+++ pkg/html/common.go
@@ -9,14 +9,16 @@ import (
 func GetSearchPostReq() (rval Node) {
 	rval = Button(Type("submit"), Text(`Generate search request`),
 		Class("rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"),
-		//hx.Include("zc"),
-		//hx.Include("ac"),
-		hx.Include("kw"),
-		//hx.Include("allzc"),
-		//hx.Include("allac"),
-		//hx.Include("allkw"),
-		//hx.Include("indivkw"),
-		hx.Post("/genqry"),
+
+		hx.Include("#zc"),
+		hx.Include("#ac"),
+		hx.Include("#kw"),
+		hx.Include("#allzc"),
+		hx.Include("#allac"),
+		hx.Include("#allkw"),
+		hx.Include("#indivkw"),
+		hx.Include("#sonly"),
+		hx.Post("/genqry"), hx.Include("*"),
 	)
 	return
 }
