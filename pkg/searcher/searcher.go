331264f3f4a7f1cb59aabadb35430f77cf4e0398
Dan Henderson <sftwrngnr66@gmail.com>
2025-02-14 22:23:16 -0700

    Updated search routine and schema.

--- pkg/searcher/searcher.go
+++ pkg/searcher/searcher.go
@@ -24,40 +24,27 @@ func (sp *SearchParms) Searchdata() ([]string, error) {
 		"google_domain": sp.SearchDomain,
 	}
 
-	search := g.NewGoogleSearch(parameter, system.GetSystemParams().GQKey)
-	data, err := search.GetJSON()
-	if err != nil {
-		fmt.Printf("Fuck chocolate shakes.\n")
-		return rval, err
-	}
-	//fmt.Printf("%v\n", data)
-	if data["organic_results"] != nil {
-		results := data["organic_results"].([]interface{})
-		fmt.Printf("Got organic results: \n")
-		for i := range len(results) {
-			if results[i] == nil {
-				continue
-			}
-			myMap := results[i].(map[string]interface{})
-			fmt.Printf("%s\n", myMap["title"].(string))
-			fmt.Printf("%s\n", myMap["link"].(string))
-			//fmt.Printf("%i\n", myMap["display_link"].(string))
-			fmt.Printf("\n")
-			rval = append(rval, myMap["link"].(string))
-		}
-	}
-
 	/*
-		results := data["organic_results"].([]interface{})
-		for i := range len(results) {
-			if results[i] != nil {
-				for k, v := range results[i].(map[string]interface{}) {
-					fmt.Printf("%s :: %v\n", k, v)
-				}
-			}
-			//rval = append(rval, results[i].(map[string]interface{})["displayed_link"].(string))
+		search := g.NewGoogleSearch(parameter, system.GetSystemParams().GQKey)
+		data, err := search.GetJSON()
+		if err != nil {
+			return rval, err
 		}
-
+		//fmt.Printf("%v\n", data)
+		if data["organic_results"] != nil {
+			results := data["organic_results"].([]interface{})
+			fmt.Printf("Got organic results: \n")
+			for i := range len(results) {
+				if results[i] == nil {
+					continue
+				}
+				myMap := results[i].(map[string]interface{})
+				fmt.Printf("%s\n", myMap["title"].(string))
+				fmt.Printf("%s\n", myMap["link"].(string))
+				fmt.Printf("\n")
+				rval = append(rval, myMap["link"].(string))
+			}
+		}
 	*/
 	return rval, err
 }
--- schema/soleirclear.sql
+++ schema/soleirclear.sql
@@ -1,26 +1,26 @@
 CREATE TABLE public.areacodes (
-                                  id bigserial NOT NULL,
-                                  code varchar NULL,
-                                  state bigint NULL,
-                                  latitude float8 NULL,
-                                  longitude float8 NULL,
-                                  created_at date NULL,
-                                  updated_at date NULL,
-                                  deleted_at date NULL,
-                                  CONSTRAINT areacodes_pk PRIMARY KEY (id)
+    id bigserial NOT NULL,
+    code varchar NULL,
+    state bigint NULL,
+    latitude float8 NULL,
+    longitude float8 NULL,
+    created_at date NULL,
+    updated_at date NULL,
+    deleted_at date NULL,
+    CONSTRAINT areacodes_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE public.states (
-                               id bigserial NOT NULL,
-                               abbrev varchar NOT NULL,
-                               "name" varchar NULL,
-                               capitol varchar NULL,
-                               region varchar NULL,
-                               created_at date NULL,
-                               updated_at date NULL,
-                               deleted_at date NULL,
-                               CONSTRAINT states_pk PRIMARY KEY (id),
-                               CONSTRAINT states_unique UNIQUE (abbrev)
+    id bigserial NOT NULL,
+    abbrev varchar NOT NULL,
+    "name" varchar NULL,
+    capitol varchar NULL,
+    region varchar NULL,
+    created_at date NULL,
+    updated_at date NULL,
+    deleted_at date NULL,
+    CONSTRAINT states_pk PRIMARY KEY (id),
+    CONSTRAINT states_unique UNIQUE (abbrev)
 );
 CREATE INDEX states_name_idx ON public.states ("name");
 
@@ -28,62 +28,109 @@ ALTER TABLE public.areacodes ADD CONSTRAINT areacodes_states_fk FOREIGN KEY (sta
 
 
 CREATE TABLE public.cities (
-                               id bigserial NOT NULL,
-                               "name" varchar NOT NULL,
-                               state bigint NOT NULL,
-                               created_at date NULL,
-                               updated_at date NULL,
-                               deleted_at date NULL,
-                               CONSTRAINT cities_pk PRIMARY KEY (id),
-                               CONSTRAINT cities_states_fk FOREIGN KEY (state) REFERENCES public.states(id) ON DELETE CASCADE ON UPDATE CASCADE
+    id bigserial NOT NULL,
+    "name" varchar NOT NULL,
+    state bigint NOT NULL,
+    created_at date NULL,
+    updated_at date NULL,
+    deleted_at date NULL,
+    CONSTRAINT cities_pk PRIMARY KEY (id),
+    CONSTRAINT cities_states_fk FOREIGN KEY (state) REFERENCES public.states(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE INDEX cities_name_idx ON public.cities ("name");
 
 CREATE TABLE public.zipcodes (
-                                id bigserial NOT NULL,
-                                zipcode varchar NULL,
-                                city bigint NULL,
-                                state bigint NULL,
-                                population bigint NULL,
-                                latitude float8 NULL,
-                                longitude float8 NULL,
-                                created_at date NULL,
-                                updated_at date NULL,
-                                deleted_at date NULL,
-                                CONSTRAINT zipcode_pk PRIMARY KEY (id),
-                                CONSTRAINT zipcode_cities_fk FOREIGN KEY (city) REFERENCES public.cities(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                                CONSTRAINT zipcode_states_fk FOREIGN KEY (state) REFERENCES public.states(id) ON DELETE CASCADE ON UPDATE CASCADE
+    id bigserial NOT NULL,
+    zipcode varchar NULL,
+    city bigint NULL,
+    state bigint NULL,
+    population bigint NULL,
+    latitude float8 NULL,
+    longitude float8 NULL,
+    created_at date NULL,
+    updated_at date NULL,
+    deleted_at date NULL,
+    CONSTRAINT zipcode_pk PRIMARY KEY (id),
+    CONSTRAINT zipcode_cities_fk FOREIGN KEY (city) REFERENCES public.cities(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT zipcode_states_fk FOREIGN KEY (state) REFERENCES public.states(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE INDEX zipcode_state_idx ON public.zipcodes (state,population);
 
 CREATE TABLE public.cityareacodes (
-                                      id bigserial NOT NULL,
-                                      areacode bigint NOT NULL,
-                                      city bigint NOT NULL,
-                                      created_at date NULL,
-                                      updated_at date NULL,
-                                      deleted_at date NULL,
-                                      CONSTRAINT cityareacodes_cities_fk FOREIGN KEY (city) REFERENCES public.cities(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                                      CONSTRAINT cityareacodes_cities_fk_1 FOREIGN KEY (city) REFERENCES public.cities(id)
+    id bigserial NOT NULL,
+    areacode bigint NOT NULL,
+    city bigint NOT NULL,
+    created_at date NULL,
+    updated_at date NULL,
+    deleted_at date NULL,
+    CONSTRAINT cityareacodes_cities_fk FOREIGN KEY (city) REFERENCES public.cities(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT cityareacodes_pk PRIMARY KEY (id)
 );
 CREATE INDEX cityareacodes_city_idx ON public.cityareacodes (city,created_at,updated_at,deleted_at);
 
 CREATE TABLE public.keywords (
-                                 id bigserial NOT NULL,
-                                 keyword varchar NULL,
-                                 created_at date NULL,
-                                 updated_at date NULL,
-                                 deleted_at date NULL,
-                                 CONSTRAINT keywords_pk PRIMARY KEY (id)
+    id bigserial NOT NULL,
+    keyword varchar NULL,
+    created_at date NULL,
+    updated_at date NULL,
+    deleted_at date NULL,
+    CONSTRAINT keywords_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE public.crawler_results (
-                                id bigserial NOT NULL,
-                                query_id bigint NULL,
-                                title varchar NULL,
-                                link varchar NULL,
-                                display_link varchar NULL,
-                                CONSTRAINT crawler_results_pk PRIMARY KEY (id)
+
+CREATE TABLE public.qry_ac (
+    id bigserial NOT NULL,
+    qry_id bigint NULL,
+    qry_ac bigint NULL,
+    CONSTRAINT qry_ac_pk PRIMARY KEY (id),
+    CONSTRAINT qry_ac_areacodes_fk FOREIGN KEY (qry_ac) REFERENCES public.areacodes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT qry_ac_query_fk FOREIGN KEY (qry_id) REFERENCES public.query(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+create table query_results
+(
+    id bigserial not null,
+    query_id  bigint,
+    title varchar,
+    link   varchar,
+    display_link varchar,
+    constraint crawler_results_pk primary key
+    constraint query_results_query_fk foreign key (query_id) references public.query(id) on update cascade on delete cascade,
+);
+
+CREATE TABLE public.qry_zips (
+    id bigserial NOT NULL,
+    query_id bigint NULL,
+    zip_id bigint NULL,
+    CONSTRAINT qry_zips_pk PRIMARY KEY (id),
+    CONSTRAINT qry_zips_query_fk FOREIGN KEY (query_id) REFERENCES public.query(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT qry_zips_zipcodes_fk FOREIGN KEY (zip_id) REFERENCES public.zipcodes(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE public.query
+(
+    id bigserial NOT NULL,
+    state bigint,
+    keyboard_ids varchar   NULL,
+    zip_ids varchar   NULL,
+    ac_ids  varchar   NULL,
+    keyword_ids varchar NULL,
+    zip_ids varchar NULL,
+    ac_ids varchar NULL,
+    CONSTRAINT query_states_fk FOREIGN KEY (state) REFERENCES public.states(id)
+);
+
+
+
+CREATE TABLE public.qry_kwds (
+    id bigserial NOT NULL,
+    query_id bigint NULL,
+    keyword_id bigint NULL,
+    CONSTRAINT qry_kwds_pk PRIMARY KEY (id),
+    CONSTRAINT qry_kwds_query_fk FOREIGN KEY (query_id) REFERENCES public.query(id),
+    CONSTRAINT qry_kwds_keywords_fk FOREIGN KEY (keyword_id) REFERENCES public.keywords(id)
 );
 
 
